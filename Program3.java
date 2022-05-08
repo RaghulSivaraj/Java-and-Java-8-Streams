b49b5d6fa2e5d94b91c5b79df4b0628b090000f3
Raghul S <raghulsiva2018@gmail.com>
2022-05-08 09:22:08 +0530

    Update Problem Statement 3

--- Program3.java
+++ Program3.java
@@ -1,3 +1,18 @@
+// Program 3
+// Given a number with perfect square n, Generate a matrix with n random numbers between 1-10 in double with maximum precision of 2. Please note, you have to check if the number is perfect square before generating the matrix.
+// Sample Input 1:
+
+// 9
+// Output 1:
+
+// 8.81 9.64 4.56
+// 2.34 2.54 2.22
+// 6.90 2.34 4.35
+
+
+
+
+
 package com.company;
 
 import java.util.Scanner;
